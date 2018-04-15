fc1521257cdab8201115e652dc486e44302d6f0c
JK Jung <jkjung13@gmail.com>
2018-04-15 18:47:09 +0800

    First working version of the train/predict scripts of the ResNet50 model

---
+++ predict_resnet50.py
@@ -0,0 +1,58 @@
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing import image
+import os
+import sys
+import glob
+import argparse
+import numpy as np
+
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    args = parser.parse_args()
+    return args
+
+
+def get_files(path):
+    if os.path.isdir(path):
+        files = glob.glob(os.path.join(path, '*'))
+    elif path.find('*') > 0:
+        files = glob.glob(path)
+    else:
+        files = [path]
+
+    files = [f for f in files if f.endswith('JPG') or f.endswith('jpg')]
+
+    if not len(files):
+        sys.exit('No images found by the given path!')
+
+    return files
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    files = get_files(args.path)
+    cls_list = ['cats', 'dogs']
+
+    # load the trained model
+    net = load_model('model-resnet50-final.h5')
+
+    # loop through all files and make predictions
+    for f in files:
+        img = image.load_img(f, target_size=(224,224))
+        if img is None:
+            continue
+        x = image.img_to_array(img)
+        x = preprocess_input(x)
+        x = np.expand_dims(x, axis=0)
+        pred = net.predict(x)[0]
+        top_inds = pred.argsort()[::-1][:5]
+        print(f)
+        for i in top_inds:
+            print('    {:.3f}  {}'.format(pred[i], cls_list[i]))
---
+++ train_resnet50.py
@@ -0,0 +1,69 @@
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import Flatten, Dense, Dropout
+from tensorflow.python.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+
+DATASET_PATH  = '/data/catsdogs/sample'
+IMAGE_SIZE    = (224, 224)
+NUM_CLASSES   = 2
+BATCH_SIZE    = 8
+FREEZE_LAYERS = 2
+NUM_EPOCHS    = 20
+WEIGHTS_FINAL = 'model-resnet50-final.h5'
+
+
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   channel_shift_range=10,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+train_batches = train_datagen.flow_from_directory(DATASET_PATH + '/train',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE)
+
+valid_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+valid_batches = valid_datagen.flow_from_directory(DATASET_PATH + '/valid',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=False,
+                                                  batch_size=BATCH_SIZE)
+
+print('****************')
+for cls, idx in train_batches.class_indices.items():
+    print('Class #{} = {}'.format(idx, cls))
+print('****************')
+
+net = ResNet50(include_top=False, weights='imagenet', input_tensor=None,
+               input_shape=(IMAGE_SIZE[0],IMAGE_SIZE[1],3))
+x = net.output
+x = Flatten()(x)
+x = Dropout(0.5)(x)
+output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
+net_final = Model(inputs=net.input, outputs=output_layer)
+for layer in net_final.layers[:FREEZE_LAYERS]:
+    layer.trainable = False
+for layer in net_final.layers[FREEZE_LAYERS:]:
+    layer.trainable = True
+net_final.compile(optimizer=Adam(lr=1e-5),
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+print(net_final.summary())
+
+net_final.fit_generator(train_batches,
+                        steps_per_epoch = train_batches.samples // BATCH_SIZE,
+                        validation_data = valid_batches, 
+                        validation_steps = valid_batches.samples // BATCH_SIZE,
+                        epochs = NUM_EPOCHS)
+
+# save trained weights
+net_final.save(WEIGHTS_FINAL)
